0ad54f8a3ead9c7256cc40fba1db99719912d0ad
agustinmiura <agustinmiura@no-reply.github.com>
2023-10-04 21:03:16 -0300

    Now checks against the quantities specified in the documentation

---
+++ src/main/java/com/example/api/enums/CellphoneNameEnum.java
@@ -0,0 +1,30 @@
+package com.example.api.enums;
+
+import java.util.Arrays;
+
+public enum CellphoneNameEnum {
+    S9("Samsung Galaxy S9"),
+    S8("Samsung Galaxy S8"),
+    NEXUS_6("Motorola Nexus 6"),
+    ONEPLUS_9("Oneplus 9"),
+    IPHONE_13("Apple iPhone 13"),
+    IPHONE_12("Apple iPhone 12"),
+    IPHONE_11("Apple iPhone 11"),
+    IPHONE_X("iPhone X"),
+    NOKIA_3310("Nokia 3310");
+
+    private String fullName;
+
+    private CellphoneNameEnum(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public static boolean validName(String phoneName) {
+        var optionalName = Arrays.stream(CellphoneNameEnum.values()).filter(cellphoneNameEnum -> cellphoneNameEnum.fullName.equals(phoneName)).findAny();
+        return optionalName.isPresent();
+    }
+}
--- src/main/java/com/example/api/service/CellphoneService.java
+++ src/main/java/com/example/api/service/CellphoneService.java
@@ -2,12 +2,17 @@ package com.example.api.service;
 
 import com.example.api.dto.BookingDto;
 import com.example.api.dto.PhoneDto;
+import com.example.api.enums.CellphoneNameEnum;
 import com.example.api.repository.IBookingRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static com.example.api.enums.CellphoneNameEnum.S8;
 
 @Slf4j
 @Service
@@ -15,15 +20,18 @@ public class CellphoneService {
 
     private IBookingRepository bookingRepository;
 
+    private Map<String, Integer> initialQtyMap = new HashMap<>();
+
     public CellphoneService(IBookingRepository bookingRepository) {
         this.bookingRepository = bookingRepository;
+        fillQtyMap();
     }
 
     public PhoneDto getPhone(String phoneName) {
         var phoneDto = new PhoneDto();
         phoneDto.setName(phoneName);
         List<BookingDto> bookings = bookingRepository.findBooking(phoneName);
-        var initialQty = 10;
+        var initialQty = initialQtyMap.get(phoneName);
         var freeQty = initialQty - bookings.size();
         var available = freeQty >= 1;
         phoneDto.setAvailable(available);
@@ -34,5 +42,13 @@ public class CellphoneService {
             phoneDto.setBookingTime(latest);
         }
         return phoneDto;
+    }
+
+    private void fillQtyMap() {
+        var list = CellphoneNameEnum.values();
+        for(var item : list) {
+            var qty = item == S8 ? 2 : 1;
+            initialQtyMap.put(item.getFullName(),qty);
+        }
     }
 }
